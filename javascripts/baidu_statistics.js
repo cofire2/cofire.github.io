71fff7762ae9f84319558610aabee3393d20da09
cofire.github.io <yli_666@163.com>
2017-08-07 21:16:35 +0800

    Update baidu_statistics.js

--- javascripts/baidu_statistics.js
+++ javascripts/baidu_statistics.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?0fabc0f4e48c8c27b8eb172bfc97dadc";
+  hm.src = "//hm.baidu.com/hm.js?9ffbaede435082141a42e41653b8b378";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
